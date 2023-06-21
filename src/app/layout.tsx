c6af2736653f8987be95ae7b36ba53da09f40a28
Daria Usatyuk <dashok.myr@gmail.com>
2023-06-21 17:04:22 -0500

    title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
 import Navbar from "@/components/Navbar";
-import {Providers} from "@/app/providers";
+import { Providers } from "@/app/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,19 +19,20 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body className={inter.className}>
-      <Providers>
-      <div className="bg-light-gray dark:bg-very-dark-blue bg-auto min-h-screen tracking-wide pb-20">
-          <div className="font-sans">
-            <div className="bg-white dark:bg-dark-blue">
-              <div className="mx-auto max-w-screen-xl px-4">
-                <Navbar />
+        <link rel="icon" href="/icons/globe.png" />
+        <Providers>
+          <div className="bg-light-gray dark:bg-very-dark-blue bg-auto min-h-screen tracking-wide pb-20">
+            <div className="font-sans">
+              <div className="bg-white dark:bg-dark-blue">
+                <div className="mx-auto max-w-screen-xl px-4">
+                  <Navbar />
+                </div>
               </div>
+              <div className="mt-6" />
+              <div className="mx-auto max-w-screen-xl px-4">{children}</div>
             </div>
-            <div className="mt-6" />
-            <div className="mx-auto max-w-screen-xl px-4">{children}</div>
           </div>
-        </div>
-      </Providers>
+        </Providers>
       </body>
     </html>
   );
